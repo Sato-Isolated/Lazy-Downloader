7af6cf00e4a6fc977253fbf743394a2964abf009
Isolated <ismyskyllz@gmail.com>
2021-07-07 19:22:37 +0200

    Add project files.

---
+++ Lazy Downloader/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace Lazy_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox20 = new System.Windows.Forms.CheckBox();
+            this.checkBox19 = new System.Windows.Forms.CheckBox();
+            this.checkBox18 = new System.Windows.Forms.CheckBox();
+            this.checkBox17 = new System.Windows.Forms.CheckBox();
+            this.checkBox16 = new System.Windows.Forms.CheckBox();
+            this.checkBox15 = new System.Windows.Forms.CheckBox();
+            this.checkBox14 = new System.Windows.Forms.CheckBox();
+            this.checkBox13 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox20);
+            this.groupBox1.Controls.Add(this.checkBox19);
+            this.groupBox1.Controls.Add(this.checkBox18);
+            this.groupBox1.Controls.Add(this.checkBox17);
+            this.groupBox1.Controls.Add(this.checkBox16);
+            this.groupBox1.Controls.Add(this.checkBox15);
+            this.groupBox1.Controls.Add(this.checkBox14);
+            this.groupBox1.Controls.Add(this.checkBox13);
+            this.groupBox1.Controls.Add(this.checkBox12);
+            this.groupBox1.Controls.Add(this.checkBox11);
+            this.groupBox1.Controls.Add(this.checkBox10);
+            this.groupBox1.Controls.Add(this.checkBox9);
+            this.groupBox1.Controls.Add(this.checkBox8);
+            this.groupBox1.Controls.Add(this.checkBox7);
+            this.groupBox1.Controls.Add(this.checkBox6);
+            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 158);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // checkBox20
+            // 
+            this.checkBox20.AutoSize = true;
+            this.checkBox20.Location = new System.Drawing.Point(302, 111);
+            this.checkBox20.Name = "checkBox20";
+            this.checkBox20.Size = new System.Drawing.Size(102, 17);
+            this.checkBox20.TabIndex = 19;
+            this.checkBox20.Text = "Process Hacker";
+            this.checkBox20.UseVisualStyleBackColor = true;
+            // 
+            // checkBox19
+            // 
+            this.checkBox19.AutoSize = true;
+            this.checkBox19.Location = new System.Drawing.Point(302, 88);
+            this.checkBox19.Name = "checkBox19";
+            this.checkBox19.Size = new System.Drawing.Size(77, 17);
+            this.checkBox19.TabIndex = 18;
+            this.checkBox19.Text = "qBittorrent ";
+            this.checkBox19.UseVisualStyleBackColor = true;
+            // 
+            // checkBox18
+            // 
+            this.checkBox18.AutoSize = true;
+            this.checkBox18.Location = new System.Drawing.Point(302, 65);
+            this.checkBox18.Name = "checkBox18";
+            this.checkBox18.Size = new System.Drawing.Size(99, 17);
+            this.checkBox18.TabIndex = 17;
+            this.checkBox18.Text = "Sandboxie Plus";
+            this.checkBox18.UseVisualStyleBackColor = true;
+            // 
+            // checkBox17
+            // 
+            this.checkBox17.AutoSize = true;
+            this.checkBox17.Location = new System.Drawing.Point(111, 111);
+            this.checkBox17.Name = "checkBox17";
+            this.checkBox17.Size = new System.Drawing.Size(57, 17);
+            this.checkBox17.TabIndex = 16;
+            this.checkBox17.Text = "Firefox";
+            this.checkBox17.UseVisualStyleBackColor = true;
+            // 
+            // checkBox16
+            // 
+            this.checkBox16.AutoSize = true;
+            this.checkBox16.Location = new System.Drawing.Point(111, 88);
+            this.checkBox16.Name = "checkBox16";
+            this.checkBox16.Size = new System.Drawing.Size(68, 17);
+            this.checkBox16.TabIndex = 15;
+            this.checkBox16.Text = "OperaGx";
+            this.checkBox16.UseVisualStyleBackColor = true;
+            // 
+            // checkBox15
+            // 
+            this.checkBox15.AutoSize = true;
+            this.checkBox15.Location = new System.Drawing.Point(111, 65);
+            this.checkBox15.Name = "checkBox15";
+            this.checkBox15.Size = new System.Drawing.Size(62, 17);
+            this.checkBox15.TabIndex = 14;
+            this.checkBox15.Text = "Chrome";
+            this.checkBox15.UseVisualStyleBackColor = true;
+            // 
+            // checkBox14
+            // 
+            this.checkBox14.AutoSize = true;
+            this.checkBox14.Location = new System.Drawing.Point(302, 42);
+            this.checkBox14.Name = "checkBox14";
+            this.checkBox14.Size = new System.Drawing.Size(102, 17);
+            this.checkBox14.TabIndex = 13;
+            this.checkBox14.Text = "Ubisoft Connect";
+            this.checkBox14.UseVisualStyleBackColor = true;
+            // 
+            // checkBox13
+            // 
+            this.checkBox13.AutoSize = true;
+            this.checkBox13.Location = new System.Drawing.Point(302, 20);
+            this.checkBox13.Name = "checkBox13";
+            this.checkBox13.Size = new System.Drawing.Size(78, 17);
+            this.checkBox13.TabIndex = 12;
+            this.checkBox13.Text = "Battle.NET";
+            this.checkBox13.UseVisualStyleBackColor = true;
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.Location = new System.Drawing.Point(185, 111);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Size = new System.Drawing.Size(111, 17);
+            this.checkBox12.TabIndex = 11;
+            this.checkBox12.Text = "Epic Games Store";
+            this.checkBox12.UseVisualStyleBackColor = true;
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(185, 89);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(53, 17);
+            this.checkBox11.TabIndex = 10;
+            this.checkBox11.Text = "Origin";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(185, 66);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(56, 17);
+            this.checkBox10.TabIndex = 9;
+            this.checkBox10.Text = "Steam";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(185, 43);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(68, 17);
+            this.checkBox9.TabIndex = 8;
+            this.checkBox9.Text = "Ccleaner";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(185, 20);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(92, 17);
+            this.checkBox8.TabIndex = 7;
+            this.checkBox8.Text = "BC Uninstaller";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(111, 43);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(54, 17);
+            this.checkBox7.TabIndex = 6;
+            this.checkBox7.Text = "Brave";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(111, 20);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(70, 17);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "Telegram";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(6, 111);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(87, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Visual Studio";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 88);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(99, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Notepad++ x64";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 65);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(88, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "WinRAR x64";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 42);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "VLC";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(62, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Discord";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(430, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(677, 171);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Lazy Downloader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox18;
+        private System.Windows.Forms.CheckBox checkBox17;
+        private System.Windows.Forms.CheckBox checkBox16;
+        private System.Windows.Forms.CheckBox checkBox15;
+        private System.Windows.Forms.CheckBox checkBox14;
+        private System.Windows.Forms.CheckBox checkBox13;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox20;
+        private System.Windows.Forms.CheckBox checkBox19;
+    }
+}
+
---
+++ Lazy Downloader/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lazy_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var path = Environment.CurrentDirectory;
+            var dl = new WebClient();
+            if (checkBox1.Checked) dl.DownloadFile("https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86", "DiscordSetup.exe");
+            if (checkBox2.Checked) dl.DownloadFile("https://get.videolan.org/vlc/3.0.16/win64/vlc-3.0.16-win64.exe", "vlc-3.0.16-win64.exe");
+            if (checkBox3.Checked) dl.DownloadFile("https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-602fr.exe", "winrar-x64-602fr.exe");
+            if (checkBox4.Checked) dl.DownloadFile("https://notepad-plus-plus.org/repository/7.x/7.0/npp.7.Installer.x64.exe", "npp.7.Installer.x64.exe");
+            if (checkBox5.Checked) dl.DownloadFile("https://download.visualstudio.microsoft.com/download/pr/9dc321fd-8a9b-47ef-98a9-af0515e08d6f/533c91be0b61f481cb93758a3c6af09cef719cedbaa6e9916232918649fd4a35/vs_Community.exe", "vs_Community.exe");
+            if (checkBox6.Checked) dl.DownloadFile("https://telegram.org/dl/desktop/win64", "TelegramSetup.exe");
+            if (checkBox7.Checked) dl.DownloadFile("https://laptop-updates.brave.com/latest/winx64", "BraveSetup.exe");
+            if (checkBox8.Checked) dl.DownloadFile("https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BE060CBB4-0849-8C90-D483-C40EF182C24A%7D%26lang%3Dfr%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe", "ChromeSetup.exe");
+            if (checkBox9.Checked) dl.DownloadFile("https://net.geo.opera.com/opera_gx/stable/windows?utm_tryagain=yes&utm_source=google_via_opera_com&utm_medium=ose&utm_campaign=(none)_via_opera_com_https&http_referrer=https%3A%2F%2Fwww.google.com%2F&utm_site=opera_com&&utm_lastpage=opera.com/computer/thanks&dl_token=27647740", "OperaGxSetup.exe");
+            if (checkBox10.Checked) dl.DownloadFile("https://cdn.stubdownloader.services.mozilla.com/builds/firefox-stub/fr/win/0eec0eb5d30526efbad3e6b369b201812546d07980318d7feef46149be820284/Firefox%20Installer.exe", "FirefoxSetup.exe");
+            if (checkBox11.Checked) dl.DownloadFile("https://dotsrc.dl.osdn.net/osdn/bulk-crap-uninstaller/75105/BCUninstaller_5.0_setup.exe", "BCUninstaller_5.0_setup.exe");
+            if (checkBox12.Checked) dl.DownloadFile("https://download.ccleaner.com/ccsetup582.exe", "ccsetup582.exe");
+            if (checkBox13.Checked) dl.DownloadFile("https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe", "SteamSetup.exe");
+            if (checkBox14.Checked) dl.DownloadFile("https://www.dm.origin.com/download", "OriginSetup.exe");
+            if (checkBox15.Checked) dl.DownloadFile("https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi", "EpicGamesLauncherInstaller.msi");
+            if (checkBox16.Checked) dl.DownloadFile("https://eu.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe&id=undefined", "Battle.net-Setup.exe");
+            if (checkBox17.Checked) dl.DownloadFile("https://ubistatic3-a.akamaihd.net/orbit/launcher_installer/UbisoftConnectInstaller.exe", "UbisoftConnectInstaller.exe");
+            if (checkBox18.Checked) dl.DownloadFile("https://github.com/sandboxie-plus/Sandboxie/releases/download/0.8.6/Sandboxie-Plus-x64-v0.8.6.exe", "Sandboxie-Plus-x64-v0.8.6.exe");
+            if (checkBox19.Checked) dl.DownloadFile("https://sourceforge.net/projects/qbittorrent/files/qbittorrent-win32/qbittorrent-4.3.6/qbittorrent_4.3.6_x64_setup.exe/download", "qbittorrent_4.3.6_x64_setup.exe");
+            if (checkBox20.Checked) dl.DownloadFile("https://sourceforge.net/projects/processhacker/files/processhacker2/processhacker-2.39-setup.exe/download", "processhacker-2.39-setup.exe");
+
+            if (File.Exists(path + "\\DiscordSetup.exe"))
+                Process.Start(path + "\\DiscordSetup.exe");
+
+            if (File.Exists(path + "\\vlc-3.0.16-win64.exe"))
+                Process.Start(path + "\\vlc-3.0.16-win64.exe");
+
+            if (File.Exists(path + "\\winrar-x64-602fr.exe"))
+                Process.Start(path + "\\winrar-x64-602fr.exe");
+
+            if (File.Exists(path + "\\npp.7.Installer.x64.exe"))
+                Process.Start(path + "\\npp.7.Installer.x64.exe");
+
+            if (File.Exists(path + "\\vs_Community.exe"))
+                Process.Start(path + "\\vs_Community.exe");
+
+            if (File.Exists(path + "\\TelegramSetup.exe"))
+                Process.Start(path + "\\TelegramSetup.exe");
+
+            if (File.Exists(path + "\\BraveSetup.exe"))
+                Process.Start(path + "\\BraveSetup.exe");
+
+            if (File.Exists(path + "\\ChromeSetup.exe"))
+                Process.Start(path + "\\ChromeSetup.exe");
+
+            if (File.Exists(path + "\\OperaGxSetup.exe"))
+                Process.Start(path + "\\OperaGxSetup.exe");
+
+            if (File.Exists(path + "\\FirefoxSetup.exe"))
+                Process.Start(path + "\\FirefoxSetup.exe");
+
+            if (File.Exists(path + "\\BCUninstaller_5.0_setup.exe"))
+                Process.Start(path + "\\BCUninstaller_5.0_setup.exe");
+
+            if (File.Exists(path + "\\ccsetup582.exe"))
+                Process.Start(path + "\\ccsetup582.exe");
+
+            if (File.Exists(path + "\\SteamSetup.exe"))
+                Process.Start(path + "\\SteamSetup.exe");
+
+            if (File.Exists(path + "\\OriginSetup.exe"))
+                Process.Start(path + "\\OriginSetup.exe");
+
+            if (File.Exists(path + "\\EpicGamesLauncherInstaller.msi"))
+                Process.Start(path + "\\EpicGamesLauncherInstaller.msi");
+
+            if (File.Exists(path + "\\Battle.net-Setup.exe"))
+                Process.Start(path + "\\Battle.net-Setup.exe");
+
+            if (File.Exists(path + "\\UbisoftConnectInstaller.exe"))
+                Process.Start(path + "\\UbisoftConnectInstaller.exe");
+
+            if (File.Exists(path + "\\Sandboxie-Plus-x64-v0.8.6.exe"))
+                Process.Start(path + "\\Sandboxie-Plus-x64-v0.8.6.exe");
+
+            if (File.Exists(path + "\\qbittorrent_4.3.6_x64_setup.exe"))
+                Process.Start(path + "\\qbittorrent_4.3.6_x64_setup.exe");
+
+            if (File.Exists(path + "\\processhacker-2.39-setup.exe"))
+                Process.Start(path + "\\processhacker-2.39-setup.exe");
+
+        }
+    }
+}
