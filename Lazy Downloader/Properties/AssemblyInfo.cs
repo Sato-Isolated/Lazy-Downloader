5eb079c397a5ca577ec1a2bc870ae3d4a11a1606
Isolated <ismyskyllz@gmail.com>
2021-07-07 19:38:07 +0200

    a

--- Lazy Downloader/Properties/AssemblyInfo.cs
+++ Lazy Downloader/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fast Downloader")]
+[assembly: AssemblyTitle("LazyDownloader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fast Downloader")]
+[assembly: AssemblyProduct("LazyDownloader")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
